81c4c6636bcc9df6eabaa781f3ba7060e88a2c4a
nhiendev <nhiennkrb@gmail.com>
2025-08-23 22:11:01 +0700

    #1 init: Install dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+require('dotenv').config();
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials:true,
+  methods:['GET','POST','PUT','DELETE']
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+
 // Cổng chạy server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Route đơn giản
 app.get('/', (req, res) => {
