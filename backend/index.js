ca6aa4f51758a00753f3bfb094f8dec309cfec48
nhiendev <nhiennkrb@gmail.com>
2025-08-27 01:38:45 +0700

    feat: add api documentation using Swagger ui

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const initRoutes = require('./src/routes/index');
-const connectionDatabase = require('./src/connect_db')
+const connectionDatabase = require('./src/connect_db');
+const { swaggerUi, swaggerSpec } = require("./swagger");
+
+
 connectionDatabase();
+
 require('dotenv').config();
 app.use(cors({
   origin: process.env.CLIENT_URL,
@@ -13,6 +17,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 // Cổng chạy server
@@ -23,4 +28,5 @@ initRoutes(app)
 // Chạy server
 app.listen(PORT, () => {
   console.log(`Server đang chạy tại http://localhost:${PORT}`);
+  console.log("Swagger docs available at http://localhost:5000/api-docs");
 });
---
+++ backend/swagger.js
@@ -0,0 +1,37 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0", // version OpenAPI
+    info: {
+      title: "My API Docs App Film", // tiêu đề tài liệu
+      version: "1.0.0",
+      description: "API documentation for my Node.js project Film",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/api/v1", // base URL cho API
+      },
+    ],
+      components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+     security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"], // chỉ định nơi swagger tìm JSDoc comments
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = { swaggerUi, swaggerSpec };
